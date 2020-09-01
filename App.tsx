dfffc025a0fb2cba02ee97299994d0e1cf4b6ac4
Dorothy Thurston <dorothy.thur@gmail.com>
2020-09-01 16:01:24 -0400

    MVP

--- App.tsx
+++ App.tsx
@@ -1,12 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, TextInput, Animated } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+const emptyString = "";
+const placeHolder = "...?";
+const duration = 2000
+const useNativeDriver = true;
+
 
 export default function App() {
+  const [value, setValue] = React.useState(emptyString)
+  const iconName = value ? "angry" : "smile"
+
+  const fadeAnim = React.useRef(new Animated.Value(1)).current;
+
+  const fadeIn = () => {
+    setValue(emptyString);
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration,
+      useNativeDriver
+    }).start();
+  };
+
+
+  const fadeOut = () => {
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration,
+      useNativeDriver
+    }).start(fadeIn);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Animated.View
+          style={[
+            {
+              opacity: fadeAnim
+            }
+          ]}
+        >
+          <TextInput
+            value={value}
+            placeholder={placeHolder}
+            style={styles.input}
+            onChangeText={setValue}
+            multiline
+          />
+        <FontAwesome5.Button name={iconName} onPress={fadeOut} style={styles.submit}/>
+      </Animated.View>
     </View>
   );
 }
@@ -18,4 +63,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    width: 300,
+    padding: 6,
+    marginBottom: 10,
+    backgroundColor: '#ecf0f1'
+  },
+  submit: {
+    width: 300,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
